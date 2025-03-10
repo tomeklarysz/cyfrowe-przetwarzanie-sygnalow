1818aa44dca281e362f0d931790ed2915e0b0529
tomeklarysz <ttomek.larysz@gmail.com>
2025-03-10 23:47:39 +0100

    probkowanie, rekonstrukcja

--- lab1/z1.m
+++ lab1/z1.m
@@ -1,7 +1,7 @@
 % A
 clear all; close all;
 A = 230; % V
-f = 50;  % Hx
+f = 50;  % Hz
 T = 0.1;
 
 fs1 = 10e3;
@@ -19,14 +19,14 @@ t3 = 0:dt3:T;
 x1 = A*sin(2*pi*f.*t1);
 x2 = A*sin(2*pi*f.*t2);
 x3 = A*sin(2*pi*f.*t3);
-% plot(t1,x1,'b-',t2,x2,'r-o',t3,x3,'k-x');
+% plot(t1,x1,'b-',t2,x2,'r-o',t3,x3,'k-x'); grid; pause;
 
 % B
 T = 1;
 fs1 = 10e3;
-fs2 = 26;
-fs3 = 25;
-fs4 = 24;
+fs2 = 51;  fs2 = 26;
+fs3 = 50;  fs3 = 25;
+fs4 = 49;  fs4 = 24;
 
 t1 = 0:1/fs1:T;
 t2 = 0:1/fs2:T;
@@ -37,13 +37,58 @@ x1 = A*sin(2*pi*f.*t1);
 x2 = A*sin(2*pi*f.*t2);
 x3 = A*sin(2*pi*f.*t3);
 x4 = A*sin(2*pi*f.*t4);
+% plot(t1,x1,'b-',t2,x2,'g-o',t3,x3,'r-o',t4,x4,'k-o'); pause;
 
-% plot(t1,x1,'b-',t2,x2,'g-o',t3,x3,'r-o',t4,x4,'k-o');
-
+% C
 fs = 100;
 T = 1;
 t = 0:1/fs:T;
-for f = 0:5:300
-    x = A*sin(2*pi*f*t);
-    plot(t,x,'b-'); pause;
-end
+obieg = 1;
+% for f = 0:5:300
+%     x = A*sin(2*pi*f*t);
+%     plot(t,x,'b-'); 
+%     title([num2str(obieg), '. obieg, f = ', num2str(f), 'Hz']); pause;
+%     obieg = obieg + 1;
+% end
+
+% x1 = A*sin(2*pi*5*t);
+% x2 = A*sin(2*pi*105*t);
+% x3 = A*sin(2*pi*205*t);
+% plot(t,x1,'b-'); hold on;
+% plot(t,x2,'r-o'); hold on;
+% plot(t,x3,'k-x'); hold off;
+
+% f = k*fs +- fx
+% te sinusoidy po spróbkowaniu są nierozróżnialne, zeby tak nie bylo
+% max czestotliwosc = polowa czestotliwosci probkowania
+
+% x1 = A*sin(2*pi*95*t);
+% x2 = A*sin(2*pi*195*t);
+% x3 = A*sin(2*pi*295*t);
+% plot(t,x1,'b-o'); hold on;
+% plot(t,x2,'ro'); hold on;
+% plot(t,x3,'kx'); hold off;
+
+% x1 = A*sin(2*pi*95*t);
+% x2 = A*sin(2*pi*105*t);
+% plot(t,x1,'b-o'); hold on;
+% plot(t,x2,'r-o'); hold on;
+
+% dla f = k*fs - fx
+% identyczne, tylko ze znakiem minus
+
+% cosinus:
+
+% x1 = A*cos(2*pi*5*t);
+% x2 = A*cos(2*pi*105*t);
+% x3 = A*cos(2*pi*205*t);
+% plot(t,x1,'b-'); hold on;
+% plot(t,x2,'r-o'); hold on;
+% plot(t,x3,'k-x'); hold off;
+
+x1 = A*cos(2*pi*95*t);
+x2 = A*cos(2*pi*105*t);
+plot(t,x1,'b-o'); hold on;
+plot(t,x2,'ro'); hold on;
+
+% dla f = k*fs - fx identyczne
---
+++ lab1/z2.m
@@ -0,0 +1,21 @@
+clear all; close all;
+A = 230; % V
+f = 50;  % Hz
+T = 0.1;
+
+fs = 200;
+dt = 1/fs;
+t = 0:dt:T;
+x = sin(2*pi*f*t);
+
+ts = 0:(1/10e3):T;
+analog = sin(2*pi*f*ts);
+
+x_odt = zeros(size(ts));
+for i = 1:length(t)
+    x_odt = x_odt + x(i) * sinc((ts - t(i))/ dt);
+end
+
+
+plot(ts,analog,'k--'); hold on;
+plot(ts,x_odt,'b');
